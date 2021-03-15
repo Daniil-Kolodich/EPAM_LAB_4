b7c8b0f8d5ef5ebea6bbed660b1cecc146dd2ae3
Danon <danik.kolodich@mail.ru>
2021-03-15 18:32:29 +0300

    done

---
+++ _Polinomial/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace _Polinomial {
+    class Polinomial {
+        private double[] coefs;
+        public Polinomial(double[] coefs) {
+            this.coefs = coefs;
+            Trim();
+        }
+        public void Trim() {
+            // Избавление от ведущих нулей
+            int maxIndex = coefs.Length - 1;
+            for (; maxIndex >= 0; maxIndex--)
+                if (coefs[maxIndex] != 0)
+                    break;
+            coefs = coefs[..(maxIndex + 1)];
+        }
+
+        public static implicit operator string(Polinomial obj) {
+            string result = string.Empty;
+            string sign;
+            string xString;
+            string num;
+            string mult;
+
+            for (int i = obj.coefs.Length - 1; i >= 0; i--) {
+                if (obj[i] == 0)
+                    continue;
+                sign = obj[i] >= 0 ? " + " : " - ";
+                num = Math.Abs(obj[i]) switch {
+                    1 when i != 0 => string.Empty,
+                    _ => $"{Math.Abs(obj[i]):#.###}",
+                };
+                mult = string.IsNullOrEmpty(num) || i == 0 ? string.Empty : "*";
+                xString = i switch {
+                    0 => string.Empty,
+                    1 => "x",
+                    _ => $"x^{i}",
+                };
+                result += $"{sign}{num}{mult}{xString}";
+            }
+            return result;
+        }
+        public static Polinomial operator +(Polinomial A, Polinomial B) {
+            // резервирование нужного количества коэффициентов
+            double[] newCoefs = new double[Math.Max(A.coefs.Length, B.coefs.Length)];
+            // получение минимальной длины среди многочленов
+            int minLength = Math.Min(A.coefs.Length, B.coefs.Length);
+            bool AIsBigger = A.coefs.Length > B.coefs.Length;
+            // выполнение операции
+            for (int i = 0; i < minLength; i++)
+                newCoefs[i] = A[i] + B[i];
+            // если остались неинициализированные данные, заполняем их
+            for (int i = minLength; i < newCoefs.Length; i++)
+                newCoefs[i] = AIsBigger ? A[i] : B[i];
+            return new Polinomial(newCoefs);
+        }
+        public static Polinomial operator -(Polinomial A, Polinomial B) {
+            double[] newCoefs = new double[Math.Max(A.coefs.Length, B.coefs.Length)];
+            int minLength = Math.Min(A.coefs.Length, B.coefs.Length);
+            bool AIsBigger = A.coefs.Length > B.coefs.Length;
+            for (int i = 0; i < minLength; i++)
+                newCoefs[i] = A[i] - B[i];
+            for (int i = minLength; i < newCoefs.Length; i++)
+                newCoefs[i] = AIsBigger ? A[i] : -B[i];
+            return new Polinomial(newCoefs);
+        }
+
+        public double this[int index] {
+            get {
+                return coefs[index];
+            }
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            Polinomial firstPolinomial = GetNewPolinomial();
+            Polinomial secondPolinomial = GetNewPolinomial();
+            
+            Console.WriteLine("Введённые многочлены : ");
+            Console.WriteLine(firstPolinomial);
+            Console.WriteLine(secondPolinomial);
+
+            Polinomial sum = firstPolinomial + secondPolinomial;
+            Console.WriteLine($"Сумма : " + sum);
+
+            Polinomial sub = firstPolinomial - secondPolinomial;
+            Console.WriteLine($"Разность : " + sub);
+        }
+        public static Polinomial GetNewPolinomial() {
+            int length;
+            while (true) {
+                try {
+                    Console.WriteLine("Введите максимальную степень многочлена : ");
+                    length = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException) {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+
+            double[] coefs = new double[++length];
+            for (int i = length - 1; i >= 0; i--) {
+                while (true) {
+                    try {
+                        Console.WriteLine($"Введите коэффициент при x ^ {i} : ");
+                        coefs[i] = double.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch (FormatException) {
+                        Console.WriteLine("Invalid input");
+                    }
+                }
+            }
+
+            Console.Clear();
+            return new Polinomial(coefs);
+        }
+    }
+
+}
---
+++ _Vectors/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace _Vectors {
+    class Vector {
+        private float x;
+        private float y;
+        private float z;
+
+        public Vector(float x, float y, float z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public void Info(string message = "vector") {
+            string result = $"Info for {message}: ";
+            Console.WriteLine($"Info for {message}: ({x},{y},{z})");
+        }
+
+        public static Vector operator +(Vector vectFirst, Vector vectSecond) {
+            return new Vector(vectFirst.x + vectSecond.x, vectFirst.y + vectSecond.y, vectFirst.z + vectSecond.z);
+        }
+
+        public static Vector operator -(Vector vectFirst, Vector vectSecond) {
+            return new Vector(vectFirst.x - vectSecond.x, vectFirst.y - vectSecond.y, vectFirst.z - vectSecond.z);
+        }
+
+        // Векторное произведение
+        public static Vector operator *(Vector vectFirst, Vector vectSecond) {
+            return new Vector(vectFirst.y * vectSecond.z - vectFirst.z * vectSecond.y,
+                                vectFirst.z * vectSecond.x - vectFirst.x * vectSecond.z,
+                                vectFirst.x * vectSecond.y - vectFirst.y * vectSecond.x);
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Vector firstVect, secondVect;
+            GetNewVector(out firstVect, nameof(firstVect));
+            GetNewVector(out secondVect, nameof(secondVect));
+
+            Vector sum = firstVect + secondVect;
+            sum.Info("sum of firstVector & secondVector");
+
+            Vector sub = firstVect - secondVect;
+            sub.Info("sub of firstVector & secondVector");
+
+            Vector mul = firstVect * secondVect;
+            sub.Info("vector mult of firstVector & secondVector");
+        }
+
+        public static void GetNewVector(out Vector vect, string message) {
+            float x, y, z;
+            while (true) {
+                Console.WriteLine($"Enter three coordinates for {message}:");
+                try {
+                    x = float.Parse(Console.ReadLine());
+                    y = float.Parse(Console.ReadLine());
+                    z = float.Parse(Console.ReadLine());
+                    vect = new Vector(x, y, z);
+                    break;
+                }
+                catch (FormatException) {
+                    Console.WriteLine("Invalid input\n");
+                }
+            }
+        }
+    }
+}
